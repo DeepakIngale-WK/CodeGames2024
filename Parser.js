d14db775868623585ef23fe5d09a6ce1668da0df
Sayak Sinha <Sayak.Sinha@wolterskluwer.com>
2024-05-21 16:41:13 +0530

    Somewhere somthing works!

--- Parser.js
+++ Parser.js
@@ -1,19 +1,24 @@
+const hashSet = new Set();
+const flowchart = `flowchart TD`;
+const connectString = `-->`;
 const sequenceDiagramHttpTemplate = `
-	<select class="dropdown">
-		<option>Node</option>
-		<option>Linking Node</option>
+	<select class="nodeDropdown">
+		<option>Start/End</option>
+		<option>Input/Output</option>
+		<option>Process</option>
+		<option>Decision</option>
 	</select>
-	<input type="text" class="text" placeholder="Node name">
-	<input type="text" class="text" placeholder="Node description">
+	<input type="text" class="nodeText" placeholder="Node description">
 
-	<input type="text" class="text" placeholder="Arrow Overhead">
+	<input type="text" class="arrowText" placeholder="Arrow Overhead">
 
-	<select class="dropdown">
-		<option>Node</option>
-		<option>Linking Node</option>
+	<select class="linkNodedropdown">
+		<option>Start/End</option>
+		<option>Input/Output</option>
+		<option>Process</option>
+		<option>Decision</option>
 	</select>
-	<input type="text" class="text" placeholder="Link Node name">
-	<input type="text" class="text" placeholder="Link Node description">
+	<input type="text" class="linkNodeText" placeholder="Link Node description">
 
 	<button class="button" onclick="addRow()">+</button>
 	<button class="button" onclick="deleteRow(this)">-</button>
@@ -23,7 +28,7 @@ const sequenceDiagramHttpTemplate = `
 function showContainer() {
 	var chartSelection = document.getElementById("chartSelection");
 	var container = document.getElementById("container");
-	if (chartSelection.value === "SequenceDiagram") {
+	if (chartSelection.value === "Flowchart") {
 		container.style.display = "block";
 		addRow();
 	} else {
@@ -44,4 +49,54 @@ function deleteRow(button) {
 	var row = button.parentNode;
 	var container = row.parentNode;
 	container.removeChild(row);
+}
+
+function parseMermaidLanguage() {
+	var $container = $("#container");
+	var $rows = $container.find(".row");
+	var mermaidLanguage = `${flowchart}\n`;
+
+	$rows.each(function () {
+		var $row = $(this);
+
+		var $nodeSelection = $row.find(".nodeDropdown").val();
+		var $nodeSelection2 = $row.find(".nodeText").val();
+		var $nodeSelection3 = $row.find(".arrowText").val();
+		var $nodeSelection4 = $row.find(".linkNodedropdown").val();
+		var $nodeSelection5 = $row.find(".linkNodeText").val();
+
+		$nodeTemplate = getDropDownTemplate($nodeSelection, $nodeSelection2);
+		$linkNodeTemplate = getDropDownTemplate($nodeSelection4, $nodeSelection5);
+
+		$arrowText = $nodeSelection3 ? connectString + `|${$nodeSelection3}|` : connectString;
+
+		mermaidLanguage += `
+		${$nodeTemplate} ${$arrowText} ${$linkNodeTemplate}
+		`;
+		mermaidLanguage += `\n`;
+	});
+
+	var $preview = $("#preview");
+	$preview.html(mermaidLanguage);
+}
+
+function getDropDownTemplate(value, description) {
+	const key = value + description.replace(/\s/g, '');;
+	
+	if (hashSet.has(key)) {
+		return key;
+	}
+	else {
+		hashSet.add(key);
+		switch (value) {
+			case "Start/End":
+				return `${key}([${description}])`;
+			case "Input/Output":
+				return `${key}[/${description}/]`;
+			case "Process":
+				return `${key}(${description})`;
+			case "Decision":
+				return `${key}{${description}}`;
+		}
+	}
 }
